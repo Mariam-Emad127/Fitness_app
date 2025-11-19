8d3143484c79d192f1356af3779831d5e8990efe
Mariam Emad <93050116+Mariam-Emad127@users.noreply.github.com>
2025-11-20 00:42:25 +0200

    chatting

--- lib/feature/chatting_page/ui/chatting_screen.dart
+++ lib/feature/chatting_page/ui/chatting_screen.dart
@@ -2,8 +2,10 @@ import 'package:fitness/core/helper/constant.dart';
 import 'package:fitness/core/theming/color.dart';
 import 'package:fitness/core/widgets/image_widget.dart';
 import 'package:fitness/feature/chatting_page/ui/widgets/ReplyCard.dart';
+import 'package:fitness/feature/chatting_page/ui/widgets/own_message.dart';
 import 'package:fitness/feature/chatting_page/ui/widgets/send_message.dart';
 import 'package:flutter/material.dart';
+
 class ChattingScreen extends StatefulWidget {
   const ChattingScreen({super.key});
 
@@ -12,85 +14,67 @@ class ChattingScreen extends StatefulWidget {
 }
 
 class _ChattingScreenState extends State<ChattingScreen> {
-    final TextEditingController _textEditingController = TextEditingController();
-     final ScrollController _controller=ScrollController(); 
+  final TextEditingController _textEditingController = TextEditingController();
+  final ScrollController _controller = ScrollController();
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-         backgroundColor: ColorsManager.darkBlue,
-      appBar: AppBar(
-       
-        backgroundColor: ColorsManager.darkBlue,
-       flexibleSpace: Container( decoration: const BoxDecoration(
-       color:  ColorsManager.mainYellow,
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(40),
-          bottomRight: Radius.circular(40),
-        ),
-      ),
-      ),
-      actions: [Padding(
-        padding: const EdgeInsets.only(right: 8.0),
-        child: IconButton(onPressed:  (){}, icon:Icon(Icons.video_call),color: Colors.white, ),
-      )],
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          color: Colors.white,
-          
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+      backgroundColor: ColorsManager.darkBlue,
+
+      body: SafeArea(
+        child: Column(
           children: [
-            Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-             children: [ 
-              ImageWidget(photo: unknownImage),
-              SizedBox(width: 10,),
-              Text(
-                "recname",
-                style: TextStyle(fontSize: 15, color: Colors.white),
-                textAlign: TextAlign.left,
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                border: Border.all(color: ColorsManager.mainYellow),
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(12),
+                ),
+                color: ColorsManager.mainYellow,
               ),
-           ] ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text("chattng with oooo", style: TextStyle(fontSize: 15)),
+                    SizedBox(width: 5),
+                    ImageWidget(photo: unknownImage),
+                    SizedBox(width: 100),
+
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 15.0),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.video_call, size: 30),
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: ReplyCard(message: "message", time: "time"),
+                  ),
+                  
+            Align(
+              alignment: Alignment.topRight,
+              child: OwnMessageCard(message: "message", time: "time"),
+            ),
+                ],
+              ),
+            ),
+
+            SendBubble(messageController: _textEditingController, onPressed:  (){})
           ],
         ),
-      ),
-      body: Stack(
-        children: [
-          Positioned(
-              top: 0,
-              bottom: 60,
-              width: 200, //size.width,
-              child: ListView.builder(
-                controller: _controller,
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                reverse: true,
-                cacheExtent: 1000,
-                itemCount:5, //messageList.length,
-                itemBuilder: (BuildContext context, int index) {
-           
-                    return FittedBox(
-                      child: ReplyCard(
-                        message: "nnnn",
-                        time:"2222" 
-                      ),
-                    );
-      
-                  }
-                //},
-              )),
-          SendBubble(
-              messageController: _textEditingController,
-              onPressed: () async {
-              
-               }),
-        ],
       ),
     );
   }
--- lib/feature/chatting_page/ui/widgets/send_message.dart
+++ lib/feature/chatting_page/ui/widgets/send_message.dart
@@ -1,3 +1,4 @@
+import 'package:fitness/core/theming/color.dart';
 import 'package:flutter/material.dart';
 
 class SendBubble extends StatelessWidget {
@@ -8,54 +9,65 @@ class SendBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
       Size size = MediaQuery.maybeOf(context)!.size;
-    return       Positioned(
+    return    
+    /*   Positioned(
             bottom: 0,
             child: Container(
               height: 60,
               color: Colors.white,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: size.width * 0.80,
-                    padding: EdgeInsets.only(left: 10, right: 5),
-                    child: TextField(
-                      controller: messageController,
-                      cursorColor: Colors.black,
-                      decoration: InputDecoration(
-                        hintText: "Message",
-                        labelStyle: TextStyle(fontSize: 15, color: Colors.black),
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
+              child: 
+              */
+              Padding(
+                padding: const EdgeInsets.only(bottom:  8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 40,
+                      width: size.width * 0.85,
+                      padding: EdgeInsets.only(left: 5, right: 5),
+                      child: 
+                      TextField(
+                        controller: messageController,
+                        cursorColor: Colors.white,
+                        decoration: InputDecoration(
+                          hintText: "Message",
+                          labelStyle: TextStyle(fontSize: 15, color: Colors.black),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        
+                          fillColor: Colors.white,
+                          focusColor: Colors.white,
+                          filled: true,
+                
+                          counterText: '',
                         ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
-                        ),
-                        disabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                        ),
-                        counterText: '',
+                        style: TextStyle(fontSize: 15),
+                        keyboardType: TextInputType.text,
+                        maxLength: 500,
                       ),
-                      style: TextStyle(fontSize: 15),
-                      keyboardType: TextInputType.text,
-                      maxLength: 500,
                     ),
-                  ),
-                  Container(
-                    width: size.width * 0.20,
-                    child: IconButton(
-                      icon: Icon(Icons.send, color: Colors.redAccent),
-                      onPressed: 
-                      //() {
-                      onPressed
-                      //  _sendMessage();
-                      //},
-                    ),
-                  )
-                ],
-              ),
-            ),
-          );
+                    Container(
+                      width: size.width * 0.15,
+                      child: IconButton(
+                        icon: Icon(Icons.send, color: ColorsManager.mainYellow),
+                        onPressed: 
+                        //() {
+                        onPressed
+                        //  _sendMessage();
+                        //},
+                      ),
+                    )
+                  ],
+                ),
+              );
+         //   ),
+          //);
        
   }
 }
