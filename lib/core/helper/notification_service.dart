df90944876a5730e3b13077cdd86c174c5c494c2
Mariam Emad <93050116+Mariam-Emad127@users.noreply.github.com>
2026-02-20 01:47:33 +0200

    notification

--- lib/core/helper/notification_service.dart
+++ lib/core/helper/notification_service.dart
@@ -1,19 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:fitness/core/helper/shared_pref_helper.dart';
 
  class Notification_service{
 final firebaseMessage=FirebaseMessaging.instance;
  
  initFCM()async{
   await firebaseMessage.requestPermission();
-
-  final fcmToken=await firebaseMessage.getToken();
-  print( "fcm$fcmToken");
-
-FirebaseMessaging.onMessage.listen((message) {
-  print("messagev${message.notification!.title}");
+try{
+  String? fcmToken=await firebaseMessage.getToken() ??"";
+  SharedPrefHelper.setData("fcm",fcmToken);
+     print( "fcm       $fcmToken");
+ } catch (e) {
+    print("FCM Error: $e");
+  }
+   FirebaseMessaging.onMessage.listen((message) {
+  print("messagev${message.notification?.title??"kkkkkkkkkkkkkk"}");
     //showNotification(message);
   });
-  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message){  print("messagev${message.notification!.title}");
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message){ 
+     print("messagev${message.notification!.title}");
 });
  /*
   final initialMessage = await _messaging.getInitialMessage();
@@ -21,9 +27,17 @@ FirebaseMessaging.onMessage.listen((message) {
     _handleBackgroundMessage(initialMessage);
   }
 */
-
-FirebaseMessaging.onBackgroundMessage((RemoteMessage message)async{  print("messagev${message.notification?.title??"kkkkkkkk"}");
  
- });
-}
+  FirebaseMessaging.onBackgroundMessage(m);
+
  }
+ 
+}
+    
+  Future<void>m(RemoteMessage message)async{ 
+        await Firebase.initializeApp();
+     print("messagev${message.notification?.title??"kkkkkkkk"}");
+ 
+ }
+
+ //fj2SesQIQsum3lVVRoO0Xj:APA91bHBUT6MjAuPT0-ppbKvsY-GBiXUjxetIVweG7_B9Ogs-XEHm2y7_eG81DC_7GOXllErDabglrwEAqDQy1wCeGLs6TzELcynP4Gn_GilKXLhrA72GAg
